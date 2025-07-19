8144de54bf501b4e658d3042be3cbe802eff3de6
andrey-aka-skif <2119163@gmail.com>
2025-07-19 21:59:45 +0300

    refactor: добавлен интерфейс IPausable
    * Добавлен интерфейс IPausable для стандартизации функций паузы, запуска, перезапуска и остановки для компонентов управления игрой.

---
+++ Assets/Project/Scripts/GameManagement/IPausable.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Project.Scripts.GameManagement
+{
+    public interface IPausable
+    {
+        bool IsPaused { get; }
+        bool IsPlaying { get; }
+
+        void Start();
+        void Pause();
+        void Restart();
+        void Stop();
+    }
+}
